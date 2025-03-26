492d5ed794ef2ede62fc92a4be0b539e10fbdd91
IHyunSu <zmfnwj119@gmail.com>
2025-03-26 19:47:12 +0900

    update Spring Boot

---
+++ src/main/java/headalSpring2025_1/hyunsu/HyunsuApplication.java
@@ -0,0 +1,13 @@
+package headalSpring2025_1.hyunsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyunsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyunsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/headalSpring2025_1/hyunsu/HyunsuApplicationTests.java
@@ -0,0 +1,13 @@
+package headalSpring2025_1.hyunsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyunsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
